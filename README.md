c4287a81813af8ca30f84638906fb719320284ac
quy_le_van <quy_le_van@bita.jp>
2022-11-10 21:01:50 +0700

    feat: implemented follow, unfollow, block and unlock features

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ hello readme.md
 
 Redis Commands
 localhost:8081
+
+Run redis serve
+redis-server
+
+Watch redis
+redis-cli ping
--- src/setupServer.ts
+++ src/setupServer.ts
@@ -22,6 +22,8 @@ import { config } from '@root/config';
 import applicationRoutes from '@root/routes';
 import { CustomError, IErrorResponse } from '@global/helpers/error-handle';
 import { SocketIOPostHandler } from '@socket/post';
+import { SocketIOFollowerHandler } from '@socket/follower';
+import { SocketIOUserHandler } from '@socket/user';
 
 const SERVER_PORT = 5500;
 const log: Logger = config.createLogger('server');
@@ -131,7 +133,11 @@ export class ChattyServer {
 
   private socketIOConnections(io: Server): void {
     const postSocketHandler: SocketIOPostHandler = new SocketIOPostHandler(io);
+    const followerSocketHandler: SocketIOFollowerHandler = new SocketIOFollowerHandler(io);
+    const userSocketHandler: SocketIOUserHandler = new SocketIOUserHandler(io);
 
     postSocketHandler.listen();
+    followerSocketHandler.listen();
+    userSocketHandler.listen();
   }
 }
