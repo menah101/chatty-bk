46cb9c3d25f93825bede41e9595266e7f701a0bc
quy_le_van <quy_le_van@bita.jp>
2022-11-10 21:45:17 +0700

    feat: implementd feature notification

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -24,6 +24,7 @@ import { CustomError, IErrorResponse } from '@global/helpers/error-handle';
 import { SocketIOPostHandler } from '@socket/post';
 import { SocketIOFollowerHandler } from '@socket/follower';
 import { SocketIOUserHandler } from '@socket/user';
+import { SocketIONotificationHandler } from '@socket/notification';
 
 const SERVER_PORT = 5500;
 const log: Logger = config.createLogger('server');
@@ -135,9 +136,11 @@ export class ChattyServer {
     const postSocketHandler: SocketIOPostHandler = new SocketIOPostHandler(io);
     const followerSocketHandler: SocketIOFollowerHandler = new SocketIOFollowerHandler(io);
     const userSocketHandler: SocketIOUserHandler = new SocketIOUserHandler(io);
+    const notificationSocketHandler: SocketIONotificationHandler = new SocketIONotificationHandler();
 
     postSocketHandler.listen();
     followerSocketHandler.listen();
     userSocketHandler.listen();
+    notificationSocketHandler.listen(io);
   }
 }
